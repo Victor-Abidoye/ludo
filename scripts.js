40ddbc87c57efeec369d7449cc61ce613977c4fc
darhmey <abidoye8224@gmail.com>
2020-10-23 21:14:12 +0100

    Drops modal if player cannot make move after rolling dice -Activates roll button for all player colors when it is current playe's turn - Modal drops when if when a player cannot move any seed --Change of player's turn now effective

--- scripts.js
+++ scripts.js
@@ -1,12 +1,9 @@
 // $(document).ready(function () {
-  // selectedColor contains an ordered array of selected color
-  let selectedColor = []
   // Activates modal to diplay form for players to choose color
-  // $('#staticBackdrop').modal('show')
+$('#staticBackdrop').modal('show')
 
   // On changing the selected option, all options should be visible and then every selected option should be hidden
   $('select').change(function () {
-    selectedColor = []
     // All options should be visible
     $('select').children().css('display', 'inline')
     // for each selected color, display is set to hide to prevent it from being chosen again
@@ -14,10 +11,6 @@
       .find('option:selected')
       .each(function () {
         var color = $(this).text()
-        // push all the colors into an array
-        if (color != '--Choose a color--') {
-          selectedColor.push(color)
-        }
         // if any option is selected and is not '--Choose a color--' hide that option
         $('select')
           .children()
@@ -29,6 +22,72 @@
       })
   })
 
+// player contains an array player color array
+var player
+// Sets the first player index to 0 at the beginning of the game
+var currentPlayer = 0
+// an array to contain the number of seeds a current player has outside the home
+var rgby = []
+// keeps the record of digits rolled on dice
+var rolled = []
+// activateSeed is set to false and is set to true to enable seed selection at the END of rollin the dice
+var activateSeed = false
+
+//setPlayers() helps fill the player array
+  function setPlayers () {
+    player = [
+      [$('#playerOne1 option:selected').text(),
+      $('#playerOne2 option:selected').text()],
+      [$('#playerTwo1 option:selected').text(),
+      $('#playerTwo2 option:selected').text()],
+      [$('#playerThree option:selected').text()],
+      [$('#playerFour option:selected').text()]
+    ]
+    // creare a cleaner player array with each array in the player array has only the player's color
+    for (i = 0; i < player.length; i++){
+      for (j = 0; j < player[i].length; j++){
+        if (player[i][j] == "--Choose a color--") {
+          player[i].splice(j, 1)
+          j--
+        }
+      }
+      //  Removes empty array from the player array
+      if (player[i].length == 0) {
+        player.splice(i, 1)
+        i--
+      }
+    }
+    // Creates the rgby array the contains the number of seed in the corridor for each player[color(s)]
+    for (k = 0; k < player.length; k++){
+      m = []
+      for (l = 0; l < player[k].length; l++){
+        m.push(0)
+      }
+      rgby.push(m)
+    }
+    console.log(player)
+    console.log(rgby)
+    nextPlayer(player[currentPlayer])
+}
+
+// The changeCurrentPlayer() passes turn to the next player in the game
+function changeCurrentPlayer () {
+  currentPlayer++
+  if (currentPlayer == player.length) {
+    currentPlayer = 0
+  }
+}
+
+// Takes in as input an array containing the home colors of the current player and activates those color for rolling
+// by hiding the roll button in each
+function nextPlayer (x) {
+  console.log(x)
+  x.forEach(element => {
+    $('.' + element).removeClass("hide")
+  });
+}
+
+
   // DICE ROLL  CONTROL
   // array of classes that controls dice dots
   var one = ['faceFive']
@@ -36,48 +95,87 @@
   var three = ['faceOne', 'faceFive', 'faceNine']
   var four = ['faceOne', 'faceThree', 'faceSeven', 'faceNine']
   var five = ['faceOne', 'faceThree', 'faceFive', 'faceSeven', 'faceNine']
-  var six = [
-    'faceOne',
-    'faceThree',
-    'faceFour',
-    'faceSix',
-    'faceSeven',
-    'faceNine'
-  ]
+  var six = ['faceOne', 'faceThree', 'faceFour', 'faceSix', 'faceSeven', 'faceNine']
 
   //now() creates a loop that generates a random number that is passed to other functions
-  function now() {
-    // Variables a and b generate are ther random numbers gernerated
-    // Variavle c is and array of varibles that are themselves array of classes that controls the dice dots
-    var a
-    var b
-    var c
-    var myVar = setInterval(() => {
-      a = Math.floor(Math.random() * 6)
-      c = Math.floor(Math.random() * 6)
-      b = [one, two, three, four, five, six]
-      roll(b[a])
-      roll1(b[c])
-    }, 100)
+$('.roll').on('click', function () {
+  // Disables the roll button imediately it is clicked
+  $('.roll').prop('disabled', true)
+  $('.roll').text('PLAYING')
+
+  // Variables a and b generate are ther random numbers gernerated
+  // Variavle c is and array of varibles that are themselves array of classes that controls the dice dots
+  var a
+  var b
+  var c
+  var myVar = setInterval(() => {
+    a = Math.floor(Math.random() * 6)
+    c = Math.floor(Math.random() * 6)
+    b = [one, two, three, four, five, six]
+    roll(b[a])
+    roll1(b[c])
+  }, 100)
+  setTimeout(() => {
+    clearInterval(myVar)
     setTimeout(() => {
-      clearInterval(myVar)
-      setTimeout(() => {
-        stop(b[a])
-        stop1(b[c])
-        console.log(a + 1)
-        console.log(c + 1)
-        // If a double 6 dice is rolled, then the dice is automatically rolled again and a small dice box is created
-        // if (a == c && a == 5) {
-        //   setTimeout(() => {
-        //     var div = `<div id="six"></div><div id="six"></div>`
-        //     document.getElementById('sixs').innerHTML =
-        //       document.getElementById('sixs').innerHTML + div
-        //     now()
-        //   }, 1000)
-        // }
-      }, 100)
-    }, 2000)
+      stop(b[a])
+      stop1(b[c])
+      rolled.push(a + 1)
+      rolled.push(c + 1)
+      console.log(rolled)
+      activateSeed = true
+      if (canMove(rgby[currentPlayer])) {
+        return
+      }
+      if (a == c && a == 5) {
+        activateSeed = false
+        nextPlayer(player[currentPlayer])
+        $('.roll').prop('disabled', false)
+        $('.roll').text('ROLL')
+        // setTimeout(() => {
+        //   var div = `<div id="six"></div><div id="six"></div>`
+        //   document.getElementById('sixs').innerHTML =
+        //     document.getElementById('sixs').innerHTML + div
+        //   now()
+        // }, 1000)
+      }
+    }, 100)
+  }, 2000)
+})
+
+// canMove() takes in as input the rgby(array containing the number of seeds each color has outside)
+// If it is not possible for the current player to make any movement, a modal is dropped and the current player is changed
+function canMove (playerSeedOut) {
+  for (i = 0; i < playerSeedOut.length; i++){
+    if (!(playerSeedOut[i] > 0 || rolled.includes(6))) {
+      $('#staticBackdrop').modal('show')
+      $('#message').removeClass('hide')
+      $('.choose').css('display', 'none')
+      $('.roll').prop('disabled', false)
+      $('.roll').text('ROLL')
+      activateSeed = false
+      player[currentPlayer].forEach(element => {
+        $('.' + element).addClass('hide')
+      });
+      rolled = []
+      changeCurrentPlayer()
+      nextPlayer(player[currentPlayer])
+      break
+    } else {
+      return true
+    }
+  };
+}
+
+// On click on any seed, check if such seed is owned by the current player befor performing any operation
+$('.inner div').on('click', function () {
+  if (
+    ($(this).hasClass(player[currentPlayer][0].toLowerCase()) ||
+    $(this).hasClass(player[currentPlayer][player[currentPlayer].length - 1].toLowerCase())) && activateSeed == true
+  ) {
+    alert()
   }
+})
 
   // the roll and roll1() receives random integer input from the now() and displays the appropriate dots on each dice
   function roll(x) {
@@ -118,7 +216,9 @@
       var p = document.getElementsByClassName(x[i])
       p[0].style.visibility = 'visible'
     }
-  }
+}
+
+
 // })
 
 
